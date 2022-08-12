db2844b62eaed97213ace98d8b6bd832d9933bcb
heyjordn <proxima.aust@gmail.com>
2022-08-11 23:15:51 -0500

    fix, deploy script and readme

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,33 +1,29 @@
+
 const production = process.env.NODE_ENV === 'production'
 const title = 'Blade Directives'
 const description = 'A quick reference of Laravel Blade Directives'
 const site = production ? 'https://laravel-vite.dev' : 'http://localhost:3005'
-const image = `${site}/banner.png`
 
-const head = [
-	['meta', { name: 'author', content: 'Enzo Innocenzi' }],
-	['meta', { name: 'keywords', content: 'laravel, vitejs, vue, react, vite, php' }],
-	['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
-	['meta', { name: 'HandheldFriendly', content: 'True' }],
-	['meta', { name: 'MobileOptimized', content: '320' }],
-	['meta', { name: 'theme-color', content: '#d8b4fe' }],
-	['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-	['meta', { name: 'twitter:site', content: site }],
-	['meta', { name: 'twitter:title', value: title }],
-	['meta', { name: 'twitter:description', value: description }],
-	['meta', { name: 'twitter:image', content: image }],
-	['meta', { property: 'og:type', content: 'website' }],
-	['meta', { property: 'og:locale', content: 'en_US' }],
-	['meta', { property: 'og:site', content: site }],
-	['meta', { property: 'og:site_name', content: title }],
-	['meta', { property: 'og:title', content: title }],
-	['meta', { property: 'og:image', content: image }],
-	['meta', { property: 'og:description', content: description }],
-]
 
 export default {
 	title,
-	head,
+	head: [
+		['meta', { name: 'author', content: 'Jordan Jones' }],
+		['meta', { name: 'keywords', content: 'laravel, blade, directives, templates, php' }],
+		['meta', { name: 'HandheldFriendly', content: 'True' }],
+		['meta', { name: 'MobileOptimized', content: '320' }],
+		['meta', { name: 'theme-color', content: '#171923' }],
+		['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+		['meta', { name: 'twitter:site', content: site }],
+		['meta', { name: 'twitter:title', value: title }],
+		['meta', { name: 'twitter:description', value: description }],
+		['meta', { property: 'og:type', content: 'website' }],
+		['meta', { property: 'og:locale', content: 'en_US' }],
+		['meta', { property: 'og:site', content: site }],
+		['meta', { property: 'og:site_name', content: title }],
+		['meta', { property: 'og:title', content: title }],
+		['meta', { property: 'og:description', content: description }],
+	],
 	description,
 	lang: 'en-US',
 	scrollOffset: 'header',
@@ -42,6 +38,10 @@ export default {
 			  ]
 			}
 		  ],
+		  socialLinks: [
+			{ icon: 'github', link: 'https://github.com/heyjordn/blade-directives' },
+			{ icon: 'twitter', link: 'https://twitter.com/heyjordn' },
+		],
 		footer: {
 			license: {
 				text: 'MIT License',
--- readme.md
+++ readme.md
@@ -2,13 +2,18 @@
 <h2 align="center">Blade Directives</h2>
 
 <p align="center">
-  <br />
   <a href="https://laravel-vite.dev"><img alt="docs" src="https://img.shields.io/badge/documentation-online-blue?logo=readthedocs&logoColor=white"></a>
-  <br />
-  <br />
 </p>
 <br />
 
-<div align="center">
-Laravel Blade Directives are pretty useful methods for working with Laravel views, this is a quick reference to all core directives.
-</div>
+>### Why?
+
+Blade Directives are pretty useful methods for working with template views, however I often find myself needing to scroll through the Laravel [docs](https://laravel.com/docs/9.x/blade#blade-directives) to find them. 
+
+This provides an easier and quicker way to find them
+
+>### Credits
+
+Credits to the Collection [docs](https://laravel.com/docs/9.x/collections#available-methods), this was really the inspiration for this format
+
+
